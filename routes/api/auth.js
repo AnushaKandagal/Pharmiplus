6748b18baba16fed4c4bcc6d8a019df19e4226e6
Anusha Kandagal <anushakandagal@gmail.com>
2021-05-30 00:29:01 +0530

    commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+//const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 //@router GET api/auth
 //@router TEST route
@@ -7,4 +10,42 @@ const router = express.Router();
 
 router.get('/',(req,res)=>res.send('auth route working...'));
 
+router.post(
+    '/',
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+        else {
+            console.log('user logged it');
+           return res.send('Logged in');
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
+  );
 module.exports = router;
